8f456d0ecbd657e4d27afc960e854c542349059f
YufeiHan <hanyufei1988@gmail.com>
2021-09-15 17:55:32 -0400

    Tweet Retrieve API with Comments, Add required_params decorator

--- tweets/api/serializers.py
+++ tweets/api/serializers.py
@@ -1,5 +1,7 @@
-from accounts.api.serializers import UserSerializerForTweet
 from rest_framework import serializers
+
+from accounts.api.serializers import UserSerializerForTweet
+from comments.api.serializers import CommentSerializer
 from tweets.models import Tweet
 
 
@@ -27,3 +29,12 @@ class TweetSerializerForCreat(serializers.ModelSerializer):
         content = validated_data['content']
         tweet = Tweet.objects.create(user=user, content=content)
         return tweet
+
+
+class TweetSerializerWithComments(TweetSerializer):
+    comments = CommentSerializer(source='comment_set', many=True)
+
+    class Meta:
+        model = Tweet
+        fields = ('id', 'user', 'comments', 'created_at', 'content')
+
